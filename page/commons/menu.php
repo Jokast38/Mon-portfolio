087282755cd0005d10c18db32cd02c54598fe75a
Jokast38 <115721772+Jokast38@users.noreply.github.com>
2023-04-17 22:18:16 +0200

    Portofolio

---
+++ page/commons/menu.php
@@ -0,0 +1,67 @@
+<nav class="navbar navbar-expand-lg bg-dark perso_size20">
+<!--bg-dark ; navbar-expend-md(medium) au lieu de large afin que le menu apparaissent dès que l'écran est à la motié de sa taille--> 
+
+  <div class="container-fluid">
+   <!-- adapter la nav a notre cas -->
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle persoColor_rose" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            L'association
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item persoColor_rose" href="#">Qui sommes-nous</a></li>
+            <li><a class="dropdown-item persoColor_rose" href="#">Partenaires</a></li>
+            <li><hr class="dropdown-divider"></li>
+          </ul>
+          <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle persoColor_orange" href="../global/pensionnaires.php" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Pensionnaires
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item persoColor_orange" href="../global/pensionnaires.php">Ils cherchent une famille</a></li>
+            <li><a class="dropdown-item persoColor_orange" href="#">Longue durée famille d'acceuil</a></li>
+            <li><a class="dropdown-item persoColor_orange" href="../global/actus.php">Les anciens</a></li>
+            <li><hr class="dropdown-divider"></li>
+          </ul>
+          <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle persoColor_green" href="../global/actus.php" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Actualités
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item persoColor_green" href="../global/actus.php">Nouvelles des adoptés</a></li>
+            <li><a class="dropdown-item persoColor_green" href="#">Evenements</a></li>
+            <li><a class="dropdown-item persoColor_green" href="#">Nos actions au quotidien</a></li>
+            <li><hr class="dropdown-divider"></li>
+          </ul>
+          <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle persoColor_rouge" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Conseils
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item persoColor_rouge" href="../Articles/temperatures.php">Températures</a></li>
+            <li><a class="dropdown-item persoColor_rouge" href="../Articles/chocolat.php">Le chocolat</a></li>
+            <li><a class="dropdown-item persoColor_rouge" href="../Articles/plantesToxiques.php">Les plantes toxiques</a></li>
+            <li><a class="dropdown-item persoColor_rouge" href="../Articles/sterilisation.php">Stérilisations</a></li>
+            <li><a class="dropdown-item persoColor_rouge" href="#">Educateur Canin</a></li>
+            <li><hr class="dropdown-divider"></li>
+          </ul>
+          <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle persoColor_bleu" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Contacts
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item persoColor_bleu" href="../Contact/contact.php">Nous contactez</a></li>
+            <li><a class="dropdown-item persoColor_bleu" href="../Contact/mentions.php">Mentions</a></li>
+            <li><a class="dropdown-item persoColor_bleu" href="../Contact/don.php">Don</a></li>
+            <li><hr class="dropdown-divider"></li>
+          </ul>
+        </li>
+            
+        </div>
+    </div>
+</nav>
